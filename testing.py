146f33e7353b7230b4ea88c518f641d141026723
Grace Gaciuki <delbrivigrace@gmail.com>
2024-05-30 17:50:02 +0300

    ML model making predictions on django web page

--- testing.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-model_path = r'C:\Users\user\djangoMLDeployment\savedModels\PID_Model.joblib'
-
-try:
-    model = joblib.load(model_path)
-    print("Model loaded successfully")
-except Exception as e:
-    print(f"Error loading model: {e}")
